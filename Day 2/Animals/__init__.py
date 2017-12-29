0dfeb446157346a53382277d722f0d82ae04e2a1
Swapnil Pote <swapnilpote@Swapnils-MacBook-Air.local>
2017-12-29 21:34:54 +0530

    Day 2 Tutorials

---
+++ Day 2/Animals/__init__.py
@@ -0,0 +1,2 @@
+from .mammals import Mammals
+from .birds import Birds
---
+++ Day 2/example.py
@@ -0,0 +1,9 @@
+# Python Module example
+
+def add(a, b):
+    result = a + b
+    return result
+
+def sub(x, y):
+    result = x - y
+    return result
